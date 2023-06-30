5406701817d76605d8410f653c1950f8ffdb0e17
Somesh Kumar <kumarsomesh.unos@gmail.com>
2023-06-30 11:40:16 +0530

    setup mongodb connection and add babel, prettier config files

---
+++ src/index.js
@@ -0,0 +1,41 @@
+// All imports
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import mongoose from "mongoose"
+
+// Config
+const app = express();
+dotenv.config();
+const environment = process.env.ENVIRONMENT;
+const { config } = await import(`./../config.${environment}.js`);
+
+const main = async () => {
+  // Mongo Connection
+  mongoose.connect(config.mongo.uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+      console.log("Error connecting to MongoDB");
+      console.log(error);
+    })
+
+  // Middlewares
+  app.use(morgan(config.morgan.loggingFormat));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.get("/healthcheck", (req, res) => {
+    res.status(200).json({ status: "OK" });
+  });
+
+  app.listen(config.port, () => {
+    console.log(`Listening on port: ${config.port}`);
+  });
+}
+
+main().catch((error) => {
+  console.log("Something went wrong in main");
+  console.log(error);
+})
