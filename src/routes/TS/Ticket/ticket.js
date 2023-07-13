58c72f5546f97366c727b48848c5ae99f621fec7
AyushJaiswal18 <ayushjaiswalnb@gmail.com>
2023-07-14 01:34:36 +0530

    added controllers for ticket and modified some user controllers

--- src/routes/TS/Ticket/ticket.js
+++ src/routes/TS/Ticket/ticket.js
@@ -4,34 +4,25 @@ import express from "express";
 // Models Import
 // import Ticket from "../../../models/Ticket.js";
 // import User from "../../../models/User.js";
+import controller from "./../../../controllers/index.js";
 
 // Config
 const router = express.Router();
 
 // Getting All Tickets (auth required)
 // Pagination, Filtering (by author, date, type, approved/not approved etc)
-router.get("/", (req, res) => {
-
-})
+router.get("/", controller.ticketController.getAllTickets);
 
 // Get specific ticket
-router.get("/:id", (req, res) => {
-
-})
+router.get("/:id", controller.ticketController.getTicket);
 
 // New ticket (auth required)
-router.post("/new", (req, res) => {
-
-})
+router.post("/new", controller.ticketController.createTicket);
 
 // Update ticket (auth required)
-router.patch("/:id/update", (req, res) => {
-
-})
+router.patch("/:id/update", controller.ticketController.updateTicket);
 
 // Delete a ticket (auth required)
-router.get("/:id/delete", (req, res) => {
-
-})
+router.get("/:id/delete", controller.ticketController.deleteTicket);
 
 export default router;
